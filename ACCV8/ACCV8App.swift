12477a2e7654b6d2d4040af11462edd012e58028
Manjinder Sandhu <sandhums@gmail.com>
2022-06-28 23:20:10 +0530

    Initial Commit

---
+++ ACCV8/ACCV8App.swift
@@ -0,0 +1,17 @@
+//
+//  ACCV8App.swift
+//  ACCV8
+//
+//  Created by Manjinder Sandhu on 28/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ACCV8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ACCV8/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ACCV8
+//
+//  Created by Manjinder Sandhu on 28/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
