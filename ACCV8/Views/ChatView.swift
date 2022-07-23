441d4518e5f65667d569b1fdd01d1d215e7ca001
Manjinder Sandhu <sandhums@gmail.com>
2022-07-23 09:35:23 +0530

    check

--- ACCV8/Views/ChatView.swift
+++ ACCV8/Views/ChatView.swift
@@ -12,11 +12,20 @@ struct ChatView: View {
     @Environment(\.realm) var realm
   
     @ObservedRealmObject var user: Reps
-
+    @State var contentHasScrolled = false
     @State private var showChatView = false
     @State private var showTab: Bool = true
     var body: some View {
+        ZStack {
+            Color("Background").ignoresSafeArea()
+            
+        ScrollView {
+            scrollDetection
+ 
         VStack {
+            Rectangle()
+                .frame(width: 100, height: 72)
+                .opacity(0)
         Text("Chat")
             Button {
            showChatView = true
@@ -24,6 +33,8 @@ struct ChatView: View {
                Text ("Start Chat")
             }
    
+        }}
+        .background(Image("Blob 1").offset(x: -50, y: -400))
         }
         .sheet(isPresented: $showChatView) {
             ConversationListView(user: user)
@@ -32,8 +43,23 @@ struct ChatView: View {
         .onAppear {
                 showChatView = true
             }
+        .overlay(NavigationBar(title: "Chat", contentHasScrolled: $contentHasScrolled))
     }
-   
+    var scrollDetection: some View {
+        GeometryReader { proxy in
+            let offset = proxy.frame(in: .named("scroll")).minY
+            Color.clear.preference(key: ScrollPreferenceKey.self, value: offset)
+        }
+        .onPreferenceChange(ScrollPreferenceKey.self) { offset in
+            withAnimation(.easeInOut) {
+                if offset < 0 {
+                    contentHasScrolled = true
+                } else {
+                    contentHasScrolled = false
+                }
+            }
+        }
+    }
 }
 
 struct ChatView_Previews: PreviewProvider {
