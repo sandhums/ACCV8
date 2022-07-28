fb5c4cfdb95a313650fbffed24ed8ca712583c25
Manjinder Sandhu <sandhums@gmail.com>
2022-07-28 20:55:00 +0530

    consumables file missing

---
+++ ACCV8/Models/Consumables.swift
@@ -0,0 +1,19 @@
+//
+//  Consumables.swift
+//  ACCV8
+//
+//  Created by Manjinder Sandhu on 28/07/22.
+//
+
+import Foundation
+import RealmSwift
+
+class Consumables: Object, ObjectKeyIdentifiable {
+    @Persisted var _id: ObjectId
+    @Persisted var category = ""
+    @Persisted var name = ""
+    @Persisted var quantity = 0
+    override static func primaryKey() -> String? {
+          return "_id"
+      }
+}
